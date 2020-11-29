ed4a9ce55b8cb7020721d6ceb7c50889a6ceb9eb
mikeKots <sk8chaki@gmail.com>
2020-11-29 10:17:33 +0300

    Second homework

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
-const userName = prompt('Hello, what is your name?');
+userOperand = function()
+{
+    const operand = prompt('Hello, what You would do with numbers?');
+    return operand;
+}()
 
-alert('Hello,' + userName + '! How are you?');
+const firstNumber = prompt('Please, set first number');
+const secondNumber = prompt('Please, set second number');
+
+switch(userOperand) {
+    case '-' :
+        console.log(`Your operation is: ${firstNumber} ${userOperand} ${secondNumber} = ${firstNumber - secondNumber}`);
+        break;
+    case '+' :
+        console.log(`Your operation is: ${firstNumber} ${userOperand} ${secondNumber} = ${+firstNumber + +secondNumber}`);
+        break;
+    case '/' :
+        console.log(`Your operation is: ${firstNumber} ${userOperand} ${secondNumber} = ${firstNumber / secondNumber}`);
+        break;
+    case '*' :
+        console.log(`Your operation is: ${firstNumber} ${userOperand} ${secondNumber} = ${firstNumber * secondNumber}`);
+        break;
+    default :
+        console.log('No acceptable operands!');
+}
