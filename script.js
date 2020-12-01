58eb142fda95b9e7a645b27897c1abdc18a60920
mikeKots <sk8chaki@gmail.com>
2020-12-01 19:24:26 +0300

    Third homework

--- script.js
+++ script.js
@@ -1,25 +1,41 @@
-userOperand = function()
-{
-    const operand = prompt('Hello, what You would do with numbers?');
+const operation = getOperation();
+
+const firstOperand = getOperand('First Operand');
+const secondOperand = getOperand('Second Operand');
+
+const result = calculate(operation, firstOperand, secondOperand);
+
+showResult(operation, firstOperand, secondOperand, result);
+
+function getOperation(){
+    let operation = prompt('What to do?', 'Like + or -');
+    while (operation != '-' && operation != '+' && operation != '/' && operation != '*') {
+        operation = prompt('Please set correct operation');
+    }
+    return operation;
+}
+
+function getOperand(operandName){
+    let operand = Number(prompt('Operand' + operandName));
+    while(isNaN(operand)) {
+        operand = Number(prompt('Please set Number'));
+    }
     return operand;
-}()
+}
 
-const firstNumber = prompt('Please, set first number');
-const secondNumber = prompt('Please, set second number');
+function calculate(operation, firstOperand, secondOperand){
+    let result;
+    switch (operation) {
+        case "+" : result = firstOperand + secondOperand; break;
+        case "-" : result = firstOperand - secondOperand; break;
+        case "/" : result = firstOperand / secondOperand; break;
+        case "*" : result = firstOperand * secondOperand; break;
+        default : result = 'unknown'
+    }
+    return result;
+}
 
-switch(userOperand) {
-    case '-' :
-        console.log(`Your operation is: ${firstNumber} ${userOperand} ${secondNumber} = ${firstNumber - secondNumber}`);
-        break;
-    case '+' :
-        console.log(`Your operation is: ${firstNumber} ${userOperand} ${secondNumber} = ${+firstNumber + +secondNumber}`);
-        break;
-    case '/' :
-        console.log(`Your operation is: ${firstNumber} ${userOperand} ${secondNumber} = ${firstNumber / secondNumber}`);
-        break;
-    case '*' :
-        console.log(`Your operation is: ${firstNumber} ${userOperand} ${secondNumber} = ${firstNumber * secondNumber}`);
-        break;
-    default :
-        console.log('No acceptable operands!');
+function showResult(operation, firstOperand, secondOperand, result){
+    console.log(`${firstOperand} ${operation} ${secondOperand} = ${result}`);
+    alert(`${firstOperand} ${operation} ${secondOperand} = ${result}`);
 }
