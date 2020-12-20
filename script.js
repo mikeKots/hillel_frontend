02428bf19ad445fa31412db64048b906001dd9f2
mikeKots <sk8chaki@gmail.com>
2020-12-20 09:26:01 +0300

    Eighth homework

--- script.js
+++ script.js
@@ -1,90 +1,31 @@
 
-const VALID_OPERATORS = ['-', '+', '/', '*'];
+const calculator = createCalculator(200);
 
-const operation = getOperation();
+console.log(calculator.sum(50)); // 250
+console.log(calculator.mult(60)); // 12000
+console.log(calculator.sub(70)); // 130
+console.log(calculator.div(4)); // 50
+console.log(calculator.set(100)); // 100
 
-const howManyOperands = numberOfOperands();
+// ======= try validation =======
 
-const finalResult = getFinalResult();
+console.log(calculator.sum(undefined)); // 200
+console.log(calculator.mult('test')); // 0
+console.log(calculator.sub('88')); // 200
+console.log(calculator.div(NaN)); // Infinity
+console.log(calculator.set(true)); // true
 
-showResult(finalResult);
-
-
-function getCalculationString(number, result, calculationString, index, operandsArray){
-    if (index != operandsArray.length -1){
-       return calculationString += `${number} ${operation} `
-    };  
-    return calculationString += `${number} = ${result}`;
-}
-
-function getResult(operation, result, number, index){
-    if (index > 0) {
-        return calculate(operation, result, number);
+function createCalculator(num) {
+    let number = validateNumber(num);
+    return {
+        sum : (n) => number + validateNumber(n),
+        mult: (n) => number * validateNumber(n),
+        sub : (n) => number - validateNumber(n),
+        div : (n) => number / validateNumber(n),
+        set : (n) => number = n,
     }
-    return number;
 }
 
-function getOperation(){
-    let operation = prompt('What to do?', 'Like + or -');
-    while (!isOperatorValid(operation)) {
-        operation = prompt('Please set correct operation');
-    }
-    return operation;
-}
-
-function getOperand(operandName){
-    let operand = Number(prompt('Set ' + operandName));
-    while(!isOperandValid(operand)) {
-        operand = Number(prompt('Please set Number'));
-    }
-    return operand;
-}
-
-function numberOfOperands(){
-    let operandNumber = Number(prompt('How many Operands You want?'));
-    while(isNumberOperandsValid(operandNumber)) {
-        operandNumber = Number(prompt('Please set correct number of operands (more than 2 and less than 5)'));
-    }
-    return operandNumber;
-}
-
-function isOperandValid(operand){
-    return !isNaN(operand) && operand > 0;
-}
-
-function isOperatorValid(operation){
-    return VALID_OPERATORS.includes(operation);
-}
-
-function isNumberOperandsValid(operandNumber){
-    return !isOperandValid(operandNumber) || !(operandNumber >= 2 && operandNumber <= 5)
-}
-
-function getFinalResult(){
-    const operandsArray = new Array(howManyOperands).fill();
-    let result = 0;
-    let calculationString = '';
-    operandsArray.forEach((_, index) => {
-        let number = getOperand('Operand ' + (index +1));
-        result = getResult(operation, result, number, index);
-        calculationString = getCalculationString(number, result, calculationString, index, operandsArray);
-    });
-    return calculationString;
-}
-
-function calculate(operation, firstOperand, secondOperand){
-    let result;
-    switch (operation) {
-        case "+" : result = firstOperand + secondOperand; break;
-        case "-" : result = firstOperand - secondOperand; break;
-        case "/" : result = firstOperand / secondOperand; break;
-        case "*" : result = firstOperand * secondOperand; break;
-        default : result = 'unknown'
-    }
-    return result;
-}
-
-function showResult(calculationString){
-    console.log(calculationString);
-    alert(calculationString);
+function validateNumber(number) {
+    return !isNaN(number) && typeof Number(number) ? number : null; 
 }
