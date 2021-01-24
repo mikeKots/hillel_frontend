a60c7d16b9fcac3753cae5570bb0415a70a1e664
mikeKots <sk8chaki@gmail.com>
2021-01-24 19:22:53 +0300

    fourteenth homework

--- script.js
+++ script.js
@@ -1,90 +1,39 @@
+'use strict';
 
-const VALID_OPERATORS = ['-', '+', '/', '*'];
-
-const operation = getOperation();
-
-const howManyOperands = numberOfOperands();
-
-const finalResult = getFinalResult();
-
-showResult(finalResult);
-
-
-function getCalculationString(number, result, calculationString, index, operandsArray){
-    if (index != operandsArray.length -1){
-       return calculationString += `${number} ${operation} `
-    };  
-    return calculationString += `${number} = ${result}`;
-}
-
-function getResult(operation, result, number, index){
-    if (index > 0) {
-        return calculate(operation, result, number);
+class Group {
+    constructor () {
+        this._studentsArr = [];
+        this.students = this._studentsArr;
     }
-    return number;
-}
 
-function getOperation(){
-    let operation = prompt('What to do?', 'Like + or -');
-    while (!isOperatorValid(operation)) {
-        operation = prompt('Please set correct operation');
+    addStudent(student) {
+        if (typeof student === 'object' && student !== null) {
+            this._studentsArr.push(student);
+        }
+        return this;
     }
-    return operation;
-}
 
-function getOperand(operandName){
-    let operand = Number(prompt('Set ' + operandName));
-    while(!isOperandValid(operand)) {
-        operand = Number(prompt('Please set Number'));
+    getAverageMark() {
+        let marks = this._studentsArr.flatMap(arr => arr.marks);
+        return Math.floor(marks.reduce((acc, mark) => acc + mark, 0) / marks.length);
     }
-    return operand;
 }
 
-function numberOfOperands(){
-    let operandNumber = Number(prompt('How many Operands You want?'));
-    while(isNumberOperandsValid(operandNumber)) {
-        operandNumber = Number(prompt('Please set correct number of operands (more than 2 and less than 5)'));
+class Student {
+    constructor(name, marks) {
+        this.name = name;
+        this.marks = marks;
     }
-    return operandNumber;
 }
 
-function isOperandValid(operand){
-    return !isNaN(operand) && operand > 0;
-}
+const feGroup = new Group();
+const firstStudent = new Student('Jimmy Joe', [10, 102, 0]);
 
-function isOperatorValid(operation){
-    return VALID_OPERATORS.includes(operation);
-}
+feGroup.addStudent(new Student('John Doe', [10, 10, 5, 10]));
+feGroup.addStudent(new Student('Alex Smith', [10, 9, 8]));
+feGroup.addStudent(new Student('Bob Johnson', [9, 10, 10, 8]));
+feGroup.addStudent(firstStudent);
 
-function isNumberOperandsValid(operandNumber){
-    return !isOperandValid(operandNumber) || !(operandNumber >= 2 && operandNumber <= 5)
-}
+console.log(feGroup.students); // [{},{},{}]
 
-function getFinalResult(){
-    const operandsArray = new Array(howManyOperands).fill();
-    let result = 0;
-    let calculationString = '';
-    operandsArray.forEach((_, index) => {
-        let number = getOperand('Operand ' + (index +1));
-        result = getResult(operation, result, number, index);
-        calculationString = getCalculationString(number, result, calculationString, index, operandsArray);
-    });
-    return calculationString;
-}
-
-function calculate(operation, firstOperand, secondOperand){
-    let result;
-    switch (operation) {
-        case "+" : result = firstOperand + secondOperand; break;
-        case "-" : result = firstOperand - secondOperand; break;
-        case "/" : result = firstOperand / secondOperand; break;
-        case "*" : result = firstOperand * secondOperand; break;
-        default : result = 'unknown'
-    }
-    return result;
-}
-
-function showResult(calculationString){
-    console.log(calculationString);
-    alert(calculationString);
-}
+console.log(feGroup.getAverageMark()); // 15
