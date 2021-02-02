b3086719fa0331a19c71e49e5ef836a81065048b
mikeKots <sk8chaki@gmail.com>
2021-02-02 21:02:12 +0300

    seventeenth home work

--- script.js
+++ script.js
@@ -1,90 +1,38 @@
+const userInfoForm = document.querySelector('#gitHubUserInfo');
+const userInput = document.querySelector('#userInfo');
+const usersListEl = document.querySelector('#gitHubUsersList');
+const templateEl = document.querySelector('#gitHubInfoUser').innerHTML;
+const USER_URL = 'https://api.github.com/users/';
 
-const VALID_OPERATORS = ['-', '+', '/', '*'];
+userInfoForm.addEventListener('submit', onTodoFormSubmit);
 
-const operation = getOperation();
-
-const howManyOperands = numberOfOperands();
-
-const finalResult = getFinalResult();
-
-showResult(finalResult);
-
-
-function getCalculationString(number, result, calculationString, index, operandsArray){
-    if (index != operandsArray.length -1){
-       return calculationString += `${number} ${operation} `
-    };  
-    return calculationString += `${number} = ${result}`;
+function onTodoFormSubmit(e) {
+    e.preventDefault();
+    const fullUserUrl = USER_URL + userInput.value;
+    fetch(fullUserUrl)
+    .then((res) => {
+        if (res.status > 399) {
+            throw res;
+        }
+        return res.json();
+    })
+    .then(renderUser)
+    .catch((err) => console.error(`Sorry could not find user: ${userInput.value} status code: ${err.status}`));
 }
 
-function getResult(operation, result, number, index){
-    if (index > 0) {
-        return calculate(operation, result, number);
-    }
-    return number;
+function renderUser(userData) {
+    const newRenderUsers = generateUsersHtml(userData);
+    usersListEl.insertAdjacentHTML('beforeend', newRenderUsers);
 }
 
-function getOperation(){
-    let operation = prompt('What to do?', 'Like + or -');
-    while (!isOperatorValid(operation)) {
-        operation = prompt('Please set correct operation');
-    }
-    return operation;
+function generateUsersHtml(userData) {
+    return templateEl.replace('{{avatar_url}}', userData.avatar_url)
+        .replace('{{login}}', userData.login)
+        .replace('{{public_repos}}', userData.public_repos)
+        .replace('{{followers}}', userData.followers)
+        .replace('{{following}}', userData.following);
 }
 
-function getOperand(operandName){
-    let operand = Number(prompt('Set ' + operandName));
-    while(!isOperandValid(operand)) {
-        operand = Number(prompt('Please set Number'));
-    }
-    return operand;
-}
-
-function numberOfOperands(){
-    let operandNumber = Number(prompt('How many Operands You want?'));
-    while(isNumberOperandsValid(operandNumber)) {
-        operandNumber = Number(prompt('Please set correct number of operands (more than 2 and less than 5)'));
-    }
-    return operandNumber;
-}
-
-function isOperandValid(operand){
-    return !isNaN(operand) && operand > 0;
-}
-
-function isOperatorValid(operation){
-    return VALID_OPERATORS.includes(operation);
-}
-
-function isNumberOperandsValid(operandNumber){
-    return !isOperandValid(operandNumber) || !(operandNumber >= 2 && operandNumber <= 5)
-}
-
-function getFinalResult(){
-    const operandsArray = new Array(howManyOperands).fill();
-    let result = 0;
-    let calculationString = '';
-    operandsArray.forEach((_, index) => {
-        let number = getOperand('Operand ' + (index +1));
-        result = getResult(operation, result, number, index);
-        calculationString = getCalculationString(number, result, calculationString, index, operandsArray);
-    });
-    return calculationString;
-}
-
-function calculate(operation, firstOperand, secondOperand){
-    let result;
-    switch (operation) {
-        case "+" : result = firstOperand + secondOperand; break;
-        case "-" : result = firstOperand - secondOperand; break;
-        case "/" : result = firstOperand / secondOperand; break;
-        case "*" : result = firstOperand * secondOperand; break;
-        default : result = 'unknown'
-    }
-    return result;
-}
-
-function showResult(calculationString){
-    console.log(calculationString);
-    alert(calculationString);
+function resetForm() {
+    userInfoForm.reset();
 }
