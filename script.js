9ae41ac59a3468939595e96635d53ce69923b5c1
mikeKots <sk8chaki@gmail.com>
2021-02-10 21:13:53 +0300

    Eighteenth homework

--- script.js
+++ script.js
@@ -1,90 +1,126 @@
+const TODOS_URL = 'https://5dd3d5ba8b5e080014dc4bfa.mockapi.io/contacts/';
 
-const VALID_OPERATORS = ['-', '+', '/', '*'];
+const nameInput = document.querySelector('#nameInput');
+const surnameInput = document.querySelector('#surnameInput');
+const telNubmerInput = document.querySelector('#telNubmerInput');
+const contactsTemplate = document.querySelector('#newContact').innerHTML;
+const todoList = document.querySelector('#records');
+const deleteBtnClass = 'delete-button';
+const createdEl = 'created-contact';
 
-const operation = getOperation();
+const todosResource = new Http(TODOS_URL);
 
-const howManyOperands = numberOfOperands();
+let currentId;
 
-const finalResult = getFinalResult();
+/**
+ * When we open the page - we get all contacts 
+ */
+fetchContacts();
 
-showResult(finalResult);
+todoList.addEventListener('click', rowClickHandler);
 
+document.getElementById('addTaskBtn').addEventListener('click', onAddBtnClick);
 
-function getCalculationString(number, result, calculationString, index, operandsArray){
-    if (index != operandsArray.length -1){
-       return calculationString += `${number} ${operation} `
-    };  
-    return calculationString += `${number} = ${result}`;
-}
-
-function getResult(operation, result, number, index){
-    if (index > 0) {
-        return calculate(operation, result, number);
+function onAddBtnClick() {
+    if (!isInputValid(nameInput.value, surnameInput.value, telNubmerInput.value)) {
+        return;
     }
-    return number;
+    
+    recordHandler(currentId, nameInput.value, surnameInput.value, telNubmerInput.value);
+    clearInput();
 }
 
-function getOperation(){
-    let operation = prompt('What to do?', 'Like + or -');
-    while (!isOperatorValid(operation)) {
-        operation = prompt('Please set correct operation');
+function recordHandler(id, name, surname, phone) {
+    let data = {
+        name: name,
+        surname: surname,
+        phone: phone
     }
-    return operation;
-}
 
-function getOperand(operandName){
-    let operand = Number(prompt('Set ' + operandName));
-    while(!isOperandValid(operand)) {
-        operand = Number(prompt('Please set Number'));
+    if (id == undefined) {
+        return addNewRecord(data)
+    } else {
+        return modifyRecord(id, data);
     }
-    return operand;
 }
 
-function numberOfOperands(){
-    let operandNumber = Number(prompt('How many Operands You want?'));
-    while(isNumberOperandsValid(operandNumber)) {
-        operandNumber = Number(prompt('Please set correct number of operands (more than 2 and less than 5)'));
-    }
-    return operandNumber;
-}
-
-function isOperandValid(operand){
-    return !isNaN(operand) && operand > 0;
-}
-
-function isOperatorValid(operation){
-    return VALID_OPERATORS.includes(operation);
-}
-
-function isNumberOperandsValid(operandNumber){
-    return !isOperandValid(operandNumber) || !(operandNumber >= 2 && operandNumber <= 5)
-}
-
-function getFinalResult(){
-    const operandsArray = new Array(howManyOperands).fill();
-    let result = 0;
-    let calculationString = '';
-    operandsArray.forEach((_, index) => {
-        let number = getOperand('Operand ' + (index +1));
-        result = getResult(operation, result, number, index);
-        calculationString = getCalculationString(number, result, calculationString, index, operandsArray);
+function addNewRecord(data) {
+    return todosResource.create(data).then((res) => {
+        const newRecord = generateContactsHtml(res);
+        records.innerHTML += newRecord;
     });
-    return calculationString;
 }
 
-function calculate(operation, firstOperand, secondOperand){
-    let result;
-    switch (operation) {
-        case "+" : result = firstOperand + secondOperand; break;
-        case "-" : result = firstOperand - secondOperand; break;
-        case "/" : result = firstOperand / secondOperand; break;
-        case "*" : result = firstOperand * secondOperand; break;
-        default : result = 'unknown'
+function modifyRecord(id, data) {
+    return todosResource.update(id, data).then((res) => {
+        removeElementById(res.id);
+        const newRecord = generateContactsHtml(res);
+        records.innerHTML += newRecord;
+    });
+}
+
+function isInputValid(name, houseNumber, telNumber) {
+    return name && houseNumber && telNumber;
+}
+
+function rowClickHandler(event) {
+    if (event.target.classList.contains(deleteBtnClass)) {
+        const recordId = event.target.parentElement.parentElement.dataset.todoId;
+        todosResource.delete(recordId).then((res) => {
+           const deleteCandidate = document.querySelector(`div[data-todo-id="${res.id}"]`);
+           deleteCandidate.remove();
+        }); 
     }
-    return result;
+
+    if (event.target.parentElement.classList.contains(createdEl)) {
+        let inputs = [];
+        Array.prototype.forEach.call(event.target.parentElement.children, (child) => {
+            if (child.children.length > 0) {
+                return
+            }
+            inputs.push(child.innerText);
+        })
+        currentId = +getId(event.target.parentElement.dataset.todoId);
+        fillInput(inputs[0], inputs[1], inputs[2]);
+    }
 }
 
-function showResult(calculationString){
-    console.log(calculationString);
-    alert(calculationString);
+function clearInput() {
+    nameInput.value = '';
+    surnameInput.value = '';
+    telNubmerInput.value = '';
 }
+
+function fillInput(name, surname, telephone) {
+    nameInput.value = name;
+    surnameInput.value = surname;
+    telNubmerInput.value = telephone;
+}
+
+function generateContactsHtml(todo) {
+    return contactsTemplate
+        .replace('{{nameInput}}', todo.name)
+        .replace('{{surname}}', todo.surname)
+        .replace('{{id}}', todo.id)
+        .replace('{{phone}}', todo.phone);
+}
+
+function renderContacts(list) {
+    const html = list.map(generateContactsHtml).join('');
+    todoList.innerHTML = html;
+}
+
+function fetchContacts(){
+    todosResource.list().then(renderContacts);
+}
+
+function getId(id) {
+    return id;
+}
+
+function removeElementById(id) {
+    debugger
+    return document.querySelector(`div[data-todo-id="${id}"]`).remove();
+}
+
+
